a14041c10b63c7f1dea2ba0a87aefc756c582266
CRaph0705 <raphael.cochrane94@gmail.com>
2026-01-21 12:23:13 +0100

    comments removed

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 10:59:27 by rcochran          #+#    #+#             */
-/*   Updated: 2026/01/20 12:38:10 by rcochran         ###   ########.fr       */
+/*   Updated: 2026/01/21 12:20:45 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,39 +32,3 @@ ScalarConverter& ScalarConverter::operator=( const ScalarConverter &cpy )
 	(void)cpy;
 	return (*this);
 }
-
-// char
-/* 
-	- is it a possible value for a char ?
-		>= 0 <= 127
-	- is it printable ?
-		whitespace, >= 32 <= 126
-	- print or error msg
-*/
-// int
-/* 
-	- is it only numeric and +/- ?
-	- is it beyond int max or int min ?
-	- print or error msg
-*/
-// float
-/* 
-	- is it only numeric and +/- ?
-	- is it beyond float max or float min ?
-	- print or error msg
-*/
-// double
-/* 
-	- is it only numeric and +/- ?
-	- is it beyond double max or double min ?
-	- print or error msg
-*/
-
-
-/* 
-	- specific cases
-	> inf, -inf
-	> inff, -inff
-	> nan
-	> nanf
-*/
