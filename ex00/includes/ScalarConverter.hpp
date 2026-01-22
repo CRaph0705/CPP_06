6077098cc5c2b9906343dea7bdcb7129ce577b2b
CRaph0705 <raphael.cochrane94@gmail.com>
2026-01-22 12:09:43 +0100

    invalid input handled

--- ex00/includes/ScalarConverter.hpp
+++ ex00/includes/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 10:54:53 by rcochran          #+#    #+#             */
-/*   Updated: 2026/01/21 14:00:54 by rcochran         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:07:02 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,59 +24,24 @@ class ScalarConverter
 		ScalarConverter( const ScalarConverter &cpy );
 		ScalarConverter &operator=( const ScalarConverter &cpy );
 	public :
-		static bool isSpecialCase(const std::string &input)
-		{
-			return (
-				input == "inf" || input == "+inf" || input == "-inf" ||
-				input == "inff" || input == "+inff"|| input == "-inff" || 
-				input == "nan" || input == "nanf"
-			);
-		}
-
-		static void handleSpecialCase(const std::string &input)
-		{
-			(void)input;
-			return ;
-		}
-
-		static bool isInvalidInput(const std::string &input)
-		{
-			(void)input;
-			return (0);
-		}
-
-		static void handleInvalidInput(const std::string &input)
-		{
-			(void)input;
-			return ;
-		}
-
 		static void convert(const std::string &input)
 		{
 			double value;
 			char *end;
+			std::string invalid = "nan";
 
-			
-			std::cout << "input given : " << input << std::endl;
-			
 			value = std::strtod(input.c_str(), &end);
-			typedef void (*Converter)(double value) ;
-			
-			std::cout << "value after strtod : " << value << std::endl;
-			std::cout << std::endl << std::endl;
-			
+			if (*end != '\0')
+				value = std::strtod(invalid.c_str(), &end);
+
+			typedef void (*Converter)(double value);
 			Converter converters[4] = {
 				&conversionToChar,
 				&conversionToInt,
 				&conversionToFloat,
 				&conversionToDouble
 			};
-			
-			if (isSpecialCase(input))
-				return (handleSpecialCase(input));
-			
-			if (isInvalidInput(input))
-				return (handleInvalidInput(input));
+
 			for (int i = 0; i < 4; i++)
 			{
 				try
