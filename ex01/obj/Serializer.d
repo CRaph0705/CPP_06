60f80596864a1dd29e9b9f89d2d54f807cae64b8
CRaph0705 <raphael.cochrane94@gmail.com>
2026-01-22 15:40:59 +0100

    ex00 finished

--- ex01/obj/Serializer.d
+++
@@ -1,3 +0,0 @@
-obj/Serializer.o: src/Serializer.cpp includes/Serializer.hpp
-
-includes/Serializer.hpp:
--- ex01/obj/main.d
+++
@@ -1,5 +0,0 @@
-obj/main.o: src/main.cpp includes/main.hpp includes/Serializer.hpp
-
-includes/main.hpp:
-
-includes/Serializer.hpp:
