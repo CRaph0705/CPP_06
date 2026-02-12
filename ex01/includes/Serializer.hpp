22160d7cf54b4bb39d0a70fc6da557c1bcc28a99
CRaph0705 <raphael.cochrane94@gmail.com>
2026-02-12 14:15:48 +0100

    function body moved in cpp file

--- ex01/includes/Serializer.hpp
+++ ex01/includes/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 12:12:45 by rcochran          #+#    #+#             */
-/*   Updated: 2026/01/26 11:02:16 by rcochran         ###   ########.fr       */
+/*   Updated: 2026/02/12 14:13:11 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,9 @@ class Serializer {
 		~Serializer( );
 		Serializer &operator=( const Serializer &cpy );
 	public:
-		static uintptr_t serialize(Data* ptr)
-		{
-			return (reinterpret_cast<uintptr_t>(ptr));
-		}
+		static uintptr_t serialize(Data* ptr);
 
-		static Data* deserialize(uintptr_t raw)
-		{
-			return (reinterpret_cast<Data*>(raw));
-		}
+		static Data* deserialize(uintptr_t raw);
 
 } ;
 
--- ex01/src/Serializer.cpp
+++ ex01/src/Serializer.cpp
@@ -18,3 +18,13 @@ Serializer & Serializer::operator=( const Serializer &cpy )
 	(void)cpy;
 	return (*this);
 }
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
+}
